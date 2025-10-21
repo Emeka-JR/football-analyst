74d89ff2c7369613a039293d85259991914361bd
Emeka-JR <emekaa919@gmail.com>
2025-10-21 21:47:23 +0100

    backup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FootballAnalystComponent } from './football-analyst/football-analyst.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FootballAnalystComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
